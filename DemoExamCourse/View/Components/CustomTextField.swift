b354b0fba196f91894966435c62a15c91bf84bb1
MKudryash <jukudryash@gmail.com>
2024-10-12 09:52:11 +0700

    add font, custom ui object

---
+++ DemoExamCourse/View/Components/CustomTextField.swift
@@ -0,0 +1,29 @@
+//
+//  CustomTextField.swift
+//  DemoExamCourse
+//
+//  Created by user on 12.10.2024.
+//
+
+import SwiftUI
+
+struct CustomTextField: View {
+    let placeholder : String
+    @Binding var text:String
+    var body: some View {
+        TextField(placeholder, text: $text)
+            .font(.custom("Roboto-Medium", size:14))
+            .frame(height: 40)
+            .padding(.leading,7)
+            .background(
+                RoundedRectangle(cornerRadius: 5)
+                    .stroke(Color.gray, lineWidth: 1)
+            )
+            .autocorrectionDisabled()
+            .textInputAutocapitalization(.never)
+    }
+}
+
+#Preview {
+    CustomTextField(placeholder: "String", text: .constant(""))
+}
