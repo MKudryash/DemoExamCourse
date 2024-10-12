c21d0e52ee6fbe4b183ca3ca30302124555d4d9e
user <user@iMac-user.local>
2024-10-12 09:38:53 +0700

    init

---
+++ DemoExamCourse/DemoExamCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoExamCourseApp.swift
+//  DemoExamCourse
+//
+//  Created by user on 12.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct DemoExamCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
